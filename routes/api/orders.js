a32512e821d2de1cd459cca1f9795b75af68009d
Ryan Yusi <ryanyusi@192-168-1-6.tpgi.com.au>
2020-04-08 00:55:03 +1000

    with sonny's ammendents

--- routes/api/orders.js
+++ routes/api/orders.js
@@ -4,95 +4,113 @@ const tempJSON = require('../../data/temp.txt')
 const fetch  = require('node-fetch')
 const asana = require('asana')
 
+const createNewTask = async (orderNumber,url)=>{
+  let newTask = {
+    "data": {
+      'name': 'Order -' +   orderNumber  + '- Ryan Yusi',
+      "approval_status": "pending",
+      "assignee_status": "upcoming",
+      "completed": false,
+      "due_at": "2020-09-15T02:06:58.147Z",
+      "external": {
+        "data": "A blob of information"
+      },
+      "html_notes": "<body>Mittens <em>really</em> likes the stuff from Humboldt.</body>",
+      "liked": true,
+      "notes": "Mittens really likes the stuff from Humboldt.",
+      "resource_subtype": "default_task",
+      "assignee": "1166576348228021",
+      "projects": "1166719723537354",
+  
+      "workspace": "1165910526209201"
+    }
+  }
 
+  let postTask = {  
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer 1/1166576348228021:547169b6b8c06f866e90aeeb4226b3c0',
+      'Host': 'app.asana.com'
+    },
+    body: JSON.stringify(newTask)}
+
+    //return fetch(url,postTask);
+    try {
+    let response = await fetch(url,postTask)
+    let data = await response.json()
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+  
+}
+
+const createNewSubtask = async (url,taskGID,itemQuantity, itemName)=>{
+  let newSubTask = {
+        "data": {
+          //"name": `${item.fulfillable_quantity} x  ${item.name}`,
+          "name": `${itemQuantity} x ${itemName}`,
+          "approval_status": "pending",
+          "assignee_status": "upcoming",
+          "completed": false,
+        
+          "due_at": "2020-09-15T02:06:58.147Z",
+          "external": {
+            "gid": `${taskGID}`,
+            "data": "A blob of information"
+          },
+          "assignee": "1166576348228021",
+          "workspace": "1165910526209201"
+        }
+      }
+      let postSubTask = {  
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer 1/1166576348228021:547169b6b8c06f866e90aeeb4226b3c0',
+          'Host': 'app.asana.com'
+        },
+        body: JSON.stringify(newSubTask)}
+
+        try {
+          let response = await fetch(url + `/${taskGID}/subtasks`,postSubTask)
+          let data = await response.json()
+          return data
+        } catch (error) {
+          console.log(error)
+        }
+}
 
 //Receive order webhook POST request for new order
-router.post('/', (req, res) =>{
-    let task_gid
-    const uri = "https://app.asana.com/api/1.0/tasks"
-    const newOrder = req.body
-    const newTask = {
-      "data": {
-        'name': 'Order -' +   newOrder.name  + '- Ryan Yusi',
-        "approval_status": "pending",
-        "assignee_status": "upcoming",
-        "completed": false,
-        "due_at": "2020-09-15T02:06:58.147Z",
-        "external": {
-          "data": "A blob of information"
-        },
-        "html_notes": "<body>Mittens <em>really</em> likes the stuff from Humboldt.</body>",
-        "liked": true,
-        "notes": "Mittens really likes the stuff from Humboldt.",
-        "resource_subtype": "default_task",
-        "assignee": "1166576348228021",
-        "projects": "1166719723537354",
-    
-        "workspace": "1165910526209201"
-      }
-    }
+router.post('/', async (req, res) =>{
+  let subTasks
+  const uri = "https://app.asana.com/api/1.0/tasks"
+  const newOrder = req.body
 
-    let postTask = {  
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer 1/1166576348228021:547169b6b8c06f866e90aeeb4226b3c0',
-        'Host': 'app.asana.com'
-      },
-      body: JSON.stringify(newTask)}
-    fetch(uri,postTask)
-    .then(response => response.json())
-    .then(data => {
-      task_gid = data.data.gid
-      console.log(task_gid)
-      newOrder.line_items.forEach((item,index) => {
-        let newSubTask = {
-           "data": {
-             "name": `${item.fulfillable_quantity} x  ${item.name}`,
-             "approval_status": "pending",
-             "assignee_status": "upcoming",
-             "completed": false,
-           
-             "due_at": "2020-09-15T02:06:58.147Z",
-             "external": {
-               "gid": `${task_gid}`,
-               "data": "A blob of information"
-             },
-             "assignee": "1166576348228021",
-             "workspace": "1165910526209201"
-           }
-         }
-         let postSubTask = {  
-           method: 'POST',
-           headers: {
-             'Accept': 'application/json',
-             'Content-Type': 'application/json',
-             'Authorization': 'Bearer 1/1166576348228021:547169b6b8c06f866e90aeeb4226b3c0',
-             'Host': 'app.asana.com'
-           },
-           body: JSON.stringify(newSubTask)}
+  //create new task and get the taskGID
+  // await createNewTask(newOrder.name,uri) 
+  // .then((data)=>{ 
+  //   taskGID = data.data.gid})
+  
+  // We don't need to use `then` anymore with await
+  const taskResponse = await createNewTask(newOrder.name,uri);
+ // console.log(taskResponse.json)
+  const taskGID = taskResponse.data.gid;
 
-         fetch(`https://app.asana.com/api/1.0/tasks/${task_gid}/subtasks`,postSubTask)
-         .then(response=>response.json)
-         .then((data)=>{console.log(data.data)})
-         .catch((error) => {
-          console.error('There has been a problem with your fetch operation:', error);
-        });
-       })
-    })
-    .catch((error) => {
-      console.error('There has been a problem with your fetch operation:', error);
-    });
+  // Loop through lineItems to create an array of fetch Promises
+  const subtaskCreationPromises = newOrder.line_items.map(item => {
+    return createNewSubtask(uri,taskGID,item.fulfillable_quantity,item.name)
+  })
+console.log(subtaskCreationPromises)
+  // wait for all fetch Promises to resolve
+  await Promise.all(subtaskCreationPromises)
 
     
-   //client.users.me()
-  //.then(function(me) {
-    // Print out your information
-    //console.log('Hello world! ' + 'My name is ' + me.name + '!');
-    
-    res.status(200).send()
-  //})
+    res.status(200).send("all good")
+
 });
 
 //fetch webhook from shopify (post)
