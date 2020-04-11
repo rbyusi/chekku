d12fc776605c5c2a5d8649e6cdcdc48dd1ee7351
Ryan Yusi <ryanyusi@192-168-1-8.tpgi.com.au>
2020-04-11 21:39:05 +1000

    removed res

--- routes/api/orders.js
+++ routes/api/orders.js
@@ -84,7 +84,6 @@ router.post("/", async (req, res) => {
 
   try {
     taskResponse = await createNewTask(newOrder.name, uri);
-    res.status(200)
   } catch (e) {
     console.error(e);
     res.status(500).send("some error");
