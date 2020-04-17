ae827a5a594cfe2e3504e6a749d1b3737ec91803
Ryan Yusi <ryanyusi@192-168-1-8.tpgi.com.au>
2020-04-18 09:59:22 +1000

    added timber and dev env console logging

--- middleware/logger_w.js
+++ middleware/logger_w.js
@@ -6,24 +6,33 @@ require('dotenv').config();
 // Create a Timber client
 const timber = new Timber(process.env.ENV_TIMBER_KEY, process.env.ENV_TIMBER_SOURCE_ID);
 
+// const logger = createLogger({
+//   transports: [
+//     // new transports.Console({ 
+//     //     level: 'debug', 
+//     //     format: format.combine(format.timestamp(), format.json()) }),
+//           // Create a Winston logger - passing in the Timber transport
+//     new TimberTransport(timber)
+//     ]
+// });
 const logger = createLogger({
-  transports: [
-    new transports.Console({ 
-        level: 'debug', 
-        format: format.combine(format.timestamp(), format.json()) }),
-          // Create a Winston logger - passing in the Timber transport
-    new TimberTransport(timber)
-    ]
-});
+    transports: [new TimberTransport(timber)]
+  });
+
+
+    logger.log({
+        level: "info", // <-- will use Timber's `info` log level,
+        format: format.combine(format.timestamp(), format.json()) });
+
 
 //
 // If we're not in production then log to the `console` with the format:
 // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
 // 
-// if (process.env.NODE_ENV !== 'production') {
-//   logger.add(new transports.Console({
-//     format: format.simple()
-//   }));
-// }
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new transports.Console({
+    format: format.combine(format.timestamp(), format.json()) 
+  }));
+}
 
 module.exports = logger;
