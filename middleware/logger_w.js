eb5fe38d0b73962957ef5e21e131b67081c757c6
Ryan Yusi <ryanyusi@192-168-1-8.tpgi.com.au>
2020-04-18 10:45:58 +1000

    trying sumologic

--- middleware/logger_w.js
+++ middleware/logger_w.js
@@ -1,8 +1,15 @@
 const {createLogger, transports, format} = require('winston');
 const { Timber } = require("@timberio/node");
 const { TimberTransport } = require("@timberio/winston");
-require('dotenv').config();
+const { SumoLogic } = require('winston-sumologic-transport');
+ 
 
+require('dotenv').config();
+const options = {
+    url: 'https://tisentat.herokuapp.com/'
+  };
+  
+  winston.add(SumoLogic, options);
 // Create a Timber client
 const timber = new Timber(process.env.ENV_TIMBER_KEY, process.env.ENV_TIMBER_SOURCE_ID);
 
@@ -29,10 +36,10 @@ const logger = createLogger({
 // If we're not in production then log to the `console` with the format:
 // `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
 // 
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(new transports.Console({
-    format: format.combine(format.timestamp(), format.json()) 
-  }));
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   logger.add(new transports.Console({
+//     format: format.combine(format.timestamp(), format.json()) 
+//   }));
+// }
 
 module.exports = logger;
